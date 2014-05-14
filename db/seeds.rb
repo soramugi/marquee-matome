25a5543acea9abf4f6b997b519f4a1e59154deb1
soramugi <soramugi.chika@gmail.com>
2014-05-14 14:45:00 +0900

    とりあえずの表示が出来るようにマスターデータの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.where(
+  provider:  'twitter',
+  uid:       68404422,
+  name:      'soramugi',
+  image_url: 'http://pbs.twimg.com/profile_images/2863509226/5ebf54d483f89f1811fc0faa57acdcbe_normal.jpeg',
+).first_or_create
+
+Site.where(
+  title: 'はらすAAまとめ',
+  url: 'http://homucifer.info/harasu/',
+  user_id: user.id,
+).first_or_create
+Site.where(
+  title: '速読ウェブサービス',
+  url: 'http://hitode909.appspot.com/speed_reading/',
+  user_id: user.id,
+).first_or_create
+Site.where(
+  title: 'marquee - hitode909の日記',
+  url: 'http://hitode909.hatenablog.com/entry/2014/03/17/201717',
+  user_id: user.id,
+).first_or_create
+Site.where(
+  title: '萌えらんくBEST100',
+  url: 'http://moe100.com/html/index.php',
+  user_id: user.id,
+).first_or_create
